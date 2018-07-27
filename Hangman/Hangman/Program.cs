976f00583355e4a77930c2a7737c46d418753fb9
Jimmy Royle <jimmy_onion@hotmail.com>
2018-07-27 15:39:42 +0100

    Added code to show the entered word but with the letters replaced with underscores

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -10,6 +10,17 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Player 1, please enter a secret word for player 2 to guess: ");
+            string secretWord = Console.ReadLine();
+            
+            var letters = secretWord.ToCharArray();
+            string hiddenWord = "_";
+
+            for (int ctr = 0; ctr < letters.Length; ctr++)
+                Console.Write(" _ ", ctr);
+
+            
+            
         }
     }
 }
