31ef88722ad92bd755b328c08bce4defc2f43ca6
Jimmy Royle <jimmy_onion@hotmail.com>
2018-08-21 21:06:37 +0100

    Program now checks to see if the word is completed and quits if true using string.Compare method

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -16,6 +16,9 @@ namespace Hangman
             Console.WriteLine("Player 1, please enter a secret word for player 2 to guess: ");
             string secretWord = Console.ReadLine();
             char[] guess = new char[secretWord.Length];
+            char[] secretChar = secretWord.ToCharArray();
+            
+
 
             // Count how many letters are in the word and then store that many underscores in the 'guess' variable
             for (int ctr = 0; ctr < secretWord.Length; ctr++)
@@ -26,24 +29,29 @@ namespace Hangman
             //Guess a letter and check the secret word for a match
             while (true)
             {
+                
+                Console.WriteLine(guess);
                 Console.WriteLine("Type a letter to guess: ");
                 char playerGuess = char.Parse(Console.ReadLine());
-                char[] secretChar = secretWord.ToCharArray();
 
                 // If playerGuess matches a letter in 'secretWord' store it in 'guess'
                 for (int c = 0; c < secretWord.Length; c++)
                 {
-                    if (guess == secretChar)
+                    string a = new string(guess);
+                    int d = string.Compare(a, secretWord);
+
+                    if (d == 0)
                     {
                         Console.WriteLine("Well done! The secret word was: " + secretWord);
+                        return;
                     }
                     
-                    else if (playerGuess == secretWord[c])
+                    else if (playerGuess == secretChar[c])
                     {
                         guess[c] = playerGuess;
                     }
                 }
-                Console.WriteLine(guess);
+                
 
             }
         }
